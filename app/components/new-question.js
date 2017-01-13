fe08f1d9f0218954db36b99f7bedc83064a24b85
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-13 08:22:58 -0800

    create new-question component with functionality to add a new question from the index.

---
+++ app/components/new-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        title: this.get('title'),
+        content: this.get('content'),
+        author: this.get('author'),
+        date: this.get('date'),
+        image: this.get('image'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
+});
